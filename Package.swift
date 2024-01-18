c1d8a461a1ffb38d269284030cc9c6f2a3a8cdd6
Tony Freeman <tonyfreeman@users.noreply.github.com>
2024-01-18 11:11:27 +0300

    [Package] Add minimum target requirements

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "JavaScriptCore",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13),
+    ],
     products: [
         .library(
             name: "SJavaScriptCore",
